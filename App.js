adb1c3da67528c1cac2cf179e0e596cb73abae05
Tome <gabrielveleci@gmail.come>
2022-10-22 02:27:03 -0300

    request

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import styles from './style.js';
+import api from './src/services/api';
+import Filmes from './src/Filmes';
+
+export default function App() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes');
+      console.log(response.data);
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item}) => <Filmes data={item} />}
+      />
+    </View>
+  );
+}

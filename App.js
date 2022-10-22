38fbcc672e136c25467f55e0656c51dad0d336b3
Tome <gabrielveleci@gmail.come>
2022-10-22 05:30:25 -0300

    Load, modal e finalizações

--- App.js
+++ App.js
@@ -1,29 +1,50 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from 'react-native';
 import styles from './style.js';
 import api from './src/services/api';
 import Filmes from './src/Filmes';
 
 export default function App() {
   const [filmes, setFilmes] = useState([]);
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
       const response = await api.get('r-api/?api=filmes');
       console.log(response.data);
       setFilmes(response.data);
+      setLoad(false);
     }
 
     loadFilmes();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={filmes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({item}) => <Filmes data={item} />}
-      />
-    </View>
-  );
+  if (load) {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        <ActivityIndicator color="#121212" size={45} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={filmes}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => <Filmes data={item} />}
+        />
+      </View>
+    );
+  }
 }
